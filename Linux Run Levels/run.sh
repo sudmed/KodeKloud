4a03c40b3f651f89299bca74e581a5c7fe075344
sudmed <pashkov.dmitriy@gmail.com>
2022-04-28 12:28:17 +0300

    Create run.sh

---
+++ Linux Run Levels/run.sh
@@ -0,0 +1,28 @@
+# Linux Run Levels
+: '
+New tools have been installed on the app server in Stratos Datacenter. Some of these tools can only be managed from the graphical user interface. 
+Therefore, there are requirements for these app servers.
+On all App servers in Stratos Datacenter change the default runlevel so that they can boot in GUI (graphical user interface) by default. 
+Please do not try to reboot these servers
+'
+
+# Чтобы узнать текущий уровень выполнения достаточно ввести команду runlevel
+runlevel
+
+# В прошлом для этого приходилось редактировать файл /etc/inittab. Вы еще можете увидеть эту практику на некоторых системах.
+vi /etc/inittab
+
+# Чтобы проверить текущий уровень выполнения по умолчанию
+systemctl get-default
+
+# Чтобы просмотреть остальные "target" и уровни выполнения, ассоциированные с ними
+# ls -l /lib/systemd/system/runlevel*
+
+# чтобы поменять уровень выполнения по умолчанию, надо создать новую символьную ссылку на интересующую нас цель systemd
+# ln -sf /lib/systemd/system/runlevel3.target /etc/systemd/system/default.target
+
+# change the run level to graphical.target
+systemctl set-default graphical.target
+systemctl start graphical.target
+systemctl status graphical.target
+systemctl get-default
