7be021da7e9da663414f68b5db3d315e8f4433b2
sudmed <pashkov.dmitriy@gmail.com>
2022-05-06 20:04:19 +0300

    Update DisableRootLogin.sh

--- Disable Root Login/DisableRootLogin.sh
+++ Disable Root Login/DisableRootLogin.sh
@@ -1,3 +1,11 @@
+# Disable Root Login
+: '
+After doing some security audits of servers, xFusionCorp Industries security team has implemented some new security policies. 
+One of them is to disable direct root login through SSH.
+Disable direct SSH root login on all app servers in Stratos Datacenter.
+'
+
+
 sudo vi /etc/ssh/sshd_config
 
 # PermitRootLogin yes
@@ -5,3 +13,10 @@ sudo vi /etc/ssh/sshd_config
 
 PermitRootLogin no
 sudo systemctl restart sshd
+
+
+# You forgot to remove the hash sign #
+```bash
+#PermitRootLogin no
+```
+So the entered value remained commented out.
