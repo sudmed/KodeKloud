7bd194d9f868a04e7416c2067a6c9c07b16a6d71
sudmed <pashkov.dmitriy@gmail.com>
2022-05-09 09:42:44 +0300

    Create script.sh

---
+++ Linux LogRotate/script.sh
@@ -0,0 +1,33 @@
+# Linux LogRotate
+
+# Install the package as per the given task
+yum install squid -y
+
+# Navigate to logrotate folder and check existing folder
+/etc/logrotate.d/
+cat /etc/logrotate.d/squid
+
+# As per the task edit log file & save the file
+vi /etc/logrotate.d/squid
+
+/var/log/squid/*.log {
+    monthly
+    rotate 3
+    compress
+    notifempty
+    missingok
+    nocreate
+    sharedscripts
+    postrotate
+      # Asks squid to reopen its logs. (logfile_rotate 0 is set in squid.conf)
+      # errors redirected to make it silent if squid is not running
+      /usr/sbin/squid -k rotate 2>/dev/null
+      # Wait a little to allow Squid to catch up before the logs is compressed
+      sleep 1
+    endscript
+}
+
+
+# Start services & check the status
+systemctl start squid
+systemctl status squid
