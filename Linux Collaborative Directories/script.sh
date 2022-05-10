a7763f48b690071838fa89bd18e95c556fe71cc3
sudmed <pashkov.dmitriy@gmail.com>
2022-05-10 16:17:29 +0300

    Update script.sh

--- Linux Collaborative Directories/script.sh
+++ Linux Collaborative Directories/script.sh
@@ -1,8 +1,10 @@
 # Linux Collaborative Directories
 : '
-The Nautilus team doesn't want its data to be accessed by any of the other groups/teams due to security reasons and want their data to be strictly accessed by the dbadmin group of the team.
+The Nautilus team doesn't want its data to be accessed by any of the other groups/teams due to security reasons and want their 
+data to be strictly accessed by the dbadmin group of the team.
 Setup a collaborative directory /dbadmin/data on Nautilus App 2 server in Stratos Datacenter.
-The directory should be group owned by the group dbadmin and the group should own the files inside the directory. The directory should be read/write/execute to the group owners, and others should not have any access.
+The directory should be group owned by the group dbadmin and the group should own the files inside the directory. 
+The directory should be read/write/execute to the group owners, and others should not have any access.
 '
 
 
