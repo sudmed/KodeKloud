24f73f41763d5af86433c45c94ad51a742afb97c
sudmed <pashkov.dmitriy@gmail.com>
2022-06-16 15:25:45 +0300

    Update script.sh

--- Git Install and Create Bare Repository/script.sh
+++ Git Install and Create Bare Repository/script.sh
@@ -1,15 +1,30 @@
 # Git Install and Create Bare Repository
 : '
-The Nautilus development team shared requirements with the DevOps team regarding new application development.—specifically, 
-they want to set up a Git repository for that project. Create a Git repository on Storage server in Stratos DC as per details given below:
+The Nautilus development team shared requirements with the DevOps team regarding new application development. 
+Specifically, they want to set up a Git repository for that project. 
+Create a Git repository on Storage server in Stratos DC as per details given below:
 Install git package using yum on Storage server.
-After that create a bare repository /opt/games.git (make sure to use exact name).
+After that create a bare repository /opt/beta.git (make sure to use exact name).
 '
 
 
-yum install git -y
-cd /opt/
-git init --bare games.git
+### 1. Logon to server
+`SSH natasha@172.16.238.15`
+```console
+Password: Bl@kW
+```
+
+### 2. Install git:
+`sudo yum install -y git`
+
+### 3. Create repo:
+`sudo mkdir -p /opt/beta.git && cd $_`
+
+### 4. Initialize git:
+`sudo git init --bare`
 
 
-# You created the repo "cluster.git" instead of 'games.git'.
+```console
+CONGRATULATIONS!!!!
+You have successfully completed the quiz. Results have been saved. Ref ID:62aae3f52c9825e8810c9631
+```
